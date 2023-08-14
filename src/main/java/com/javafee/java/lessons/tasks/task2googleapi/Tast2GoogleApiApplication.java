e5747e3be279735e13926ab0d271929fad4d44ff
tolkien111 <tolkien111@wp.pl>
2023-08-14 13:42:40 +0200

    #4 tast 2 google api - init commit

---
+++ src/main/java/com/javafee/java/lessons/tasks/task2googleapi/Tast2GoogleApiApplication.java
@@ -0,0 +1,13 @@
+package com.javafee.java.lessons.tasks.task2googleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tast2GoogleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tast2GoogleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javafee/java/lessons/tasks/task2googleapi/Tast2GoogleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafee.java.lessons.tasks.task2googleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tast2GoogleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
