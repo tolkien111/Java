edf16ad321a26243010b7a3d0d17c6c8b9cff504
tolkien111 <tolkien111@wp.pl>
2023-09-13 14:29:24 +0200

    #11 - validations and exceptionhandlers - refactor code

--- src/main/java/com/javafee/java/lessons/tasks/task2googleapi/utils/PresentResultRunner.java
+++ src/main/java/com/javafee/java/lessons/tasks/task2googleapi/utils/PresentResultRunner.java
@@ -1,12 +1,10 @@
 package com.javafee.java.lessons.tasks.task2googleapi.utils;
 
-import com.javafee.java.lessons.tasks.task2googleapi.controller.CustomGlobalExceptionHandler;
 import com.javafee.java.lessons.tasks.task2googleapi.service.LocationService;
 import com.javafee.java.lessons.tasks.task2googleapi.service.dto.location.LocationView;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
