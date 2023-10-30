a1918b723ced7c5380e27fa8322d8c8ef35c5086
tolkien111 <tolkien111@wp.pl>
2023-10-30 11:56:47 +0100

    #11 - main - add status in GoogleApiGeocodingStatus

--- src/main/java/com/javafee/java/lessons/tasks/task2googleapi/service/validation/enums/GoogleApiGeocodingStatus.java
+++ src/main/java/com/javafee/java/lessons/tasks/task2googleapi/service/validation/enums/GoogleApiGeocodingStatus.java
@@ -1,5 +1,5 @@
 package com.javafee.java.lessons.tasks.task2googleapi.service.validation.enums;
 
 public enum GoogleApiGeocodingStatus {
-    OK, ZERO_RESULTS, OVER_DAILY_LIMIT, OVER_QUERY_LIMIT
+    OK, ZERO_RESULTS, OVER_DAILY_LIMIT, OVER_QUERY_LIMIT, REQUEST_DENIED
 }
