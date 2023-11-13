09f32869f8450e1c697f2bd55d1a44153b8b1f4a
tolkien111 <tolkien111@wp.pl>
2023-11-13 13:25:44 +0100

    #11 - 12-setup-circle - add validate access method in GoogleApiResponseValidator

--- src/main/java/com/javafee/java/lessons/tasks/task2googleapi/utils/PresentResultRunner.java
+++ src/main/java/com/javafee/java/lessons/tasks/task2googleapi/utils/PresentResultRunner.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
